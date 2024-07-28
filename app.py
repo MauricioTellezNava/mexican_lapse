1621822e2a59f208bdd9ce96b1905dff719972af
Mauricio PC gamer <mauricio_tellez@politicas.unam.mx>
2024-07-28 14:31:43 -0600

    Versión inicial del código fuente

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import datetime
+
+
+app = Flask(__name__)
+
+# Variables globales
+empresa = "Mexican Lapse"
+contador_visitas = 0
+
+@app.route('/')
+def index():
+    global contador_visitas
+    fecha_hora_actual = datetime.datetime.now()
+    contador_visitas += 1
+    return render_template('index.html', empresa=empresa,
+                           fecha_hora_actual=fecha_hora_actual,
+                           contador_visitas=contador_visitas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
